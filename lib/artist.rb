5a09c261665edf094b65be08079e9e39d5baf3ce
Michael Penn <mwpenn94@gmail.com>
2021-02-21 02:54:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -38,6 +38,6 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    self.find(artist_name) ? self.find(artist_name) : self.create(artist_name)
+    self.find(artist_name) ? self.find(artist_name) : self.new(artist_name)
   end
 end
